9c018a0c80fb027cd1be51a86d40df86cefd09e6
FriedrichRavaDev <fede07.ravasio@gmail.com>
2025-11-27 00:03:21 +0100

    I tried...

--- Movie.js
+++ Movie.js
@@ -1,23 +1,25 @@
-getMovies();
-async function getMovies() {
-    let text = document.getElementById("myText").innerHTML;
-    let title = document.getElementById("Cerca").innerText;
+//getMovies();
+async function getMovies(e) {
+    e.preventDefault();
+    let text = document.getElementById('myText').innerText;
+    let title = document.getElementById('Cerca').value;
     const apikey = "abdfd060"
     const url = `http://www.omdbapi.com/?&apikey=${apikey}&s=${title}`;
     try {
       const response = await fetch(url) //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-      .then(res => {
+      .then((res) => {
 
         if (!res.ok)
         throw new Error(`Response status: ${res.status}`);
 
         return res.json();
       });
-      text = JSON.stringify(response);
+      text = JSON.stringify(response, null, 2);
       var imdbID = response.imdbID; 
       } 
     catch (error) {
         console.error(error.message);
     }
-    console.log("dopo esecuzione")
+    console.log("dopo esecuzione");
+    console.log(response);
   }
