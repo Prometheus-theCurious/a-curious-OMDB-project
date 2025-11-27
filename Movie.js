907120f87785a8474e372590a4073bbffd14feff
RAVASIO.21114 <RAVASIO.21114@LABI1-13PC06>
2025-11-27 14:00:19 +0100

    IT WORKSSS!!! (search finally displays Objects)

--- Movie.js
+++ Movie.js
@@ -1,8 +1,9 @@
-//getMovies();
 async function getMovies(e) {
     e.preventDefault();
-    let text = document.getElementById('myText').innerText;
+    let text = document.getElementById('myText');
     let title = document.getElementById('Cerca').value;
+    let imdbID;
+    let Poster;
     const apikey = "abdfd060"
     const url = `http://www.omdbapi.com/?&apikey=${apikey}&s=${title}`;
     try {
@@ -14,12 +15,46 @@ async function getMovies(e) {
 
         return res.json();
       });
-      text = JSON.stringify(response, null, 2);
-      var imdbID = response.imdbID; 
+
+      text.textContent = JSON.stringify(response, null, 2);
+      imdbID = response.Search.imdbID; // imdbID is currently an object containig all the id's
+      Poster = response.Search.Poster;
       } 
     catch (error) {
         console.error(error.message);
     }
-    console.log("dopo esecuzione");
-    console.log(response);
+
+    //console.log("dopo esecuzione");
+    //console.log(response);
+
+// - - - - - -  - - -  - - - -  - -
+
+const urlID = `http://www.omdbapi.com/?&apikey=${apikey}&i=${imdbID}`;
+
+imdbID.forEach(movie => {
+  const imageURL = movie.Poster;
+  const title = movie.Title;
+  const year = movie.Year;
+  const type = movie.Type;
+});
+
+  // OMDB uses 'Poster' for the image URL
+  
+
+/*try {
+  const response = await fetch(urlID)
+  .then((res) => {
+
+    if (!res.ok)
+    throw new Error(`Response status: ${res.status}`);
+
+    return res.json();
+  });
+
+  } 
+catch (error) {
+    console.error(error.message);
+ }
+*/
+
   }
